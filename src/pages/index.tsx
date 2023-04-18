3bd90d257fb2d9f72e7525f64f2301ce9f96d326
Caro20 <carolina.segura.cordero@est.una.ac.cr>
2023-04-17 21:17:58 -0600

    Sección de información personal
    Se añade la sección de presentación personal, con una breve descripción de mi persona y el vínculo al perfil de LinkedIn.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,15 +10,25 @@ export default function Home() {
 
       <header>
 
-        <a className="menu">Start</a>
-        <a className="menu">Personal introduction</a>
-        <a className="menu">Completed projects</a>
-        <a className="menu">Call to action</a>
-        <a className="menu">Contacts and social media</a>
+        <button className="menu">Start</button>
+        <button className="menu">Personal introduction</button>
+        <button className="menu">Completed projects</button>
+        <button className="menu">Call to action</button>
+        <button className="menu">Contacts and social media</button>
 
       </header>
       <main>
-
+        <div>
+          <h1 className='title'>About me</h1>
+          <p className='description'>
+          My name is Carolina and I work in software development. I studied Computer Systems Engineering at 
+          the Universidad Nacional de Costa Rica. I consider the skills I learned in that career to be my main hard skills. 
+          However, in this section you will find a link to my LinkedIn profile so that you can learn more about me 
+          and discover that I not only love software development, but I also enjoy many other things and 
+          have more skills and hobbies. You will be able to learn about my academic background.
+          </p>
+          <a href="https://www.linkedin.com/in/carolina-segura-cordero-855483212/">Link to LinkedIn</a>
+        </div>
       </main>
 
 
